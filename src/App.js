4672f0a9ac049bdccbe7685e2c7a4811538971cd
Pau Llinas Amat <pllinas@intricom.es>
2023-06-28 16:08:25 +0200

    feat: :sparkles: Episode 7: useState exploration

--- src/App.js
+++ src/App.js
@@ -1,7 +1,39 @@
+import { useState } from 'react';
+
 function App() {
+  const [userQuery, setUserQuery] = useState('');
+
+  const searchQuery = () => {
+    window.open(`https://google.com/search?q=${userQuery}`, '_blank');
+  }
+
+  const handleKeyDown = event => {
+    if (event.key === 'Enter') {
+      searchQuery();
+    }
+  }
+
+  const updateUserQuery = event => {
+    console.log('userQuery: ', userQuery);
+
+    setUserQuery(event.target.value);
+  }
+
   return (
     <div className="App">
-      App
+      <h1>Hello Pau!</h1>
+      <div className='form'>
+        <input
+          value={userQuery}
+          onChange={updateUserQuery}
+          onKeyDown={handleKeyDown}
+        />
+        <button
+          onClick={searchQuery}
+        >
+          Search
+        </button>
+      </div>
     </div>
   );
 }
