7d67117d82ef31e7e2d1dd35f918305546360fdc
Pau Llinas Amat <pllinas@intricom.es>
2023-07-05 14:56:06 +0200

    feat: :sparkles: Implementation reducer in react-app

--- src/Tasks.js
+++ src/Tasks.js
@@ -1,5 +1,44 @@
-import { useEffect, useState } from "react";
+import { useEffect, useReducer, useState } from "react";
 import uuid from 'uuid/v4';
+
+const initialTasksState = {
+    tasks: [],
+    completedTasks: []
+}
+
+const TYPES = {
+    ADD_TASK: 'ADD_TASK',
+    COMPLETE_TASK: 'COMPLETE_TASK',
+    DELETE_TASK: 'DELETE_TASK'
+}
+
+const tasksReducer = (state, action) => {
+    console.log('state', state, 'action', action);
+
+    switch (action.type) {
+        case TYPES.ADD_TASK:
+            return {
+                ...state,
+                tasks: [...state.tasks, action.task]
+            };
+        case TYPES.COMPLETE_TASK:
+            const { completedTask } = action;
+
+            return {
+                ...state,
+                completedTasks: [...state.completedTasks, completedTask],
+                tasks: state.tasks.filter(t => t.id !== completedTask.id)
+            }
+        case TYPES.DELETE_TASK:
+            return {
+                ...state,
+                completedTasks: state.completedTasks.filter(t => t.id !== action.task.id)
+            }
+        default:
+            return state;
+    }
+
+}
 
 const TASKS_STORAGE_KEY = 'TASKS_STORAGE_KEY';
 
@@ -12,18 +51,15 @@ const storeTasks = ({ tasks, completedTasks }) => {
 
 const readStoredTasks = () => {
     const tasksMap = JSON.parse(localStorage.getItem(TASKS_STORAGE_KEY));
-    return tasksMap ? tasksMap : { tasks: [], completedTasks: [] };
+    return tasksMap ? tasksMap : initialTasksState;
 }
 
 function Tasks() {
     const [taskText, setTaskText] = useState('');
     const storedTasks = readStoredTasks();
-    const [tasks, setTasks] = useState(storedTasks.tasks);
-    const [completedTasks, setCompletedTasks] = useState(storedTasks.completedTasks);
 
-    useEffect(() => {
-        console.log("storedTasks: ", storedTasks);
-    }, [storedTasks]);
+    const [state, dispatch] = useReducer(tasksReducer, storedTasks);
+    const { tasks, completedTasks } = state;
 
     useEffect(() => {
         storeTasks({ tasks, completedTasks });
@@ -34,20 +70,16 @@ function Tasks() {
     }
 
     const addTask = () => {
-        setTasks([...tasks, { taskText, id: uuid() }]);
+        dispatch({ type: TYPES.ADD_TASK, task: { taskText, id: uuid() } });
     }
 
     const completeTask = completedTask => () => {
-        setCompletedTasks([...completedTasks, completedTask]);
-        setTasks(tasks.filter(task => task.id !== completedTask.id));
+        dispatch({ type: TYPES.COMPLETE_TASK, completedTask });
     }
 
     const delteTask = task => () => {
-        setCompletedTasks(completedTasks.filter(t => t.id !== task.id));
+        dispatch({ type: TYPES.DELETE_TASK, task });
     }
-
-    console.log('tasks', tasks);
-    console.log('completedTasks', completedTasks);
 
     return (
         <div>
